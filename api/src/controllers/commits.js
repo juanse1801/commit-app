b0d6dc9b3081537fee45ea0389e8275f80ce7a00
juanse1801 <juansala_98@hotmail.com>
2022-01-17 08:53:22 -0500

    add git pull on commit controller

--- api/src/controllers/commits.js
+++ api/src/controllers/commits.js
@@ -2,6 +2,12 @@ import { exec } from "child_process";
 
 const getAllCommits = async (req, res, next) => {
   try {
+    // To get the last changes
+    exec("git pull origin withoutGitHubApi", (err) => {
+      if (err) {
+        console.log(err);
+      }
+    });
     exec(
       'git log --all --pretty="format:%H%n%C(white) %an %nDate:   %ad%n%n%w(0,4,4)%B%n"',
       (err, stdout) => {
